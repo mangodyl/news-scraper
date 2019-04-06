8f50c7f9989d7bf43dea6e53d29cb0a0a2810060
mango dyl <dylanbonds@hotmail.com>
2019-04-06 12:41:28 -0500

    unsave working

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,8 +1,14 @@
 const db = require('../models');
 
 module.exports = function(app) {
-    app.patch("/api/articles", (req, res) => {
+    app.patch("/api/save", (req, res) => {
         db.Article.updateOne({ _id: req.body.id }, {$set: {isSaved: true}}, {}, data => {
+            res.json(data)
+        });
+    });
+
+    app.patch("/api/unsave", (req, res) => {
+        db.Article.updateOne({ _id: req.body.id }, {$set: {isSaved: false}}, {}, data => {
             res.json(data)
         });
     });
